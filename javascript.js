ae8a8764a8982ed5b7a3f9f8c3e730b379cda2fb
Hugo Leung <132837475+hugounique@users.noreply.github.com>
2024-02-20 01:08:16 +0800

    Finish the function of adding a new book to the display

--- javascript.js
+++ javascript.js
@@ -2,20 +2,57 @@ const myLibrary = [];
 const cardsContainer = document.querySelector('.cards-container');
 const dialog = document.querySelector('dialog');
 const addBookButton = document.querySelector(".add-book-button");
-const closeButton = document.querySelector("close");
+const closeDialogButton = document.querySelector(".close-dialog");
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const pageNumberInput = document.getElementById('pages');
+const readInput = document.getElementById('read');
+const submitButton = document.querySelector('input[type=submit]');
+const read = false;
 
 function Book(title, author, number, read) {
     this.title = title;
-	this.author = author;
-	this.number = number;
-	this.read = read;
+    this.author = author;
+    this.number = number;
+    this.read = read;
 }
 
 Book.prototype.addBookToLibrary = function () {
-	myLibrary.push(this);
+    myLibrary.push(this);
 }
 
-function displayBook () {
+Book.prototype.displaybook = function () {
+    let card = document.createElement('div');
+    let title = document.createElement('div');
+    let author = document.createElement('div');
+    let number = document.createElement('div');
+    let buttonsContainer = document.createElement('div');
+    let readButton = document.createElement('button');
+    card.classList.add('card');
+    title.classList.add('title');
+    author.classList.add('author');
+    number.classList.add('page-number');
+    readButton.classList.add('read');
+    buttonsContainer.classList.add('buttons-container');
+    title.textContent = this.title;
+    author.textContent = this.author;
+    number.textContent = this.number + ' pages';
+
+    if (this.read === 'true') {
+        readButton.textContent = 'read';
+    } else {
+        readButton.textContent = 'not read';
+    }
+    
+    buttonsContainer.appendChild(readButton);
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(number);
+    card.appendChild(buttonsContainer);
+    cardsContainer.appendChild(card);
+}
+
+function displayBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
         let card = document.createElement('div');
         let title = document.createElement('div');
@@ -42,17 +79,33 @@ function displayBook () {
     }
 }
 
-let bookOne = new Book('Apple','Hugo','123','read');
-let bookTwo = new Book ('Orange', 'Mac', '23', 'not read');
+let bookOne = new Book('Apple', 'Hugo', '123', 'read');
+let bookTwo = new Book('Orange', 'Mac', '23', 'not read');
 
 bookOne.addBookToLibrary();
 bookTwo.addBookToLibrary();
-displayBook();
+displayBooks();
 
 addBookButton.addEventListener("click", () => {
     dialog.showModal();
-  });
+});
 
-  closeButton.addEventListener("click", () => {
+closeDialogButton.addEventListener("click", (event) => {
+    event.preventDefault();
     dialog.close();
-  });
+});
+
+submitButton.addEventListener("click", showResult, false);
+readInput.addEventListener("change", function () {
+    this.value = this.checked ? !read : read;
+})
+
+function showResult(event) {
+    event.preventDefault();
+    dialog.close();
+    readInput.value = readInput.checked ? !read : read;
+    let book = new Book(titleInput.value, authorInput.value, pageNumberInput.value, readInput.value);
+    book.addBookToLibrary();
+    book.displaybook();
+    console.log(myLibrary);
+}
