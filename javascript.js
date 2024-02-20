85b708a88cadc8b5611e27e68f2221a5d2cb2119
Hugo Leung <132837475+hugounique@users.noreply.github.com>
2024-02-20 11:55:23 +0800

    Complete delete button function

--- javascript.js
+++ javascript.js
@@ -17,34 +17,49 @@ function Book(title, author, number, read) {
     this.read = read;
 }
 
-Book.prototype.addBookToLibrary = function () {
-    myLibrary.push(this);
+function addBookToLibrary (obj) {
+    myLibrary.push(obj);
+    displaybook(obj);
 }
 
-Book.prototype.displaybook = function () {
+function displaybook (obj) {
     let card = document.createElement('div');
+    card.dataset.number = myLibrary.length - 1;
     let title = document.createElement('div');
     let author = document.createElement('div');
     let number = document.createElement('div');
     let buttonsContainer = document.createElement('div');
     let readButton = document.createElement('button');
+    let deleteButton = document.createElement('button');
     card.classList.add('card');
     title.classList.add('title');
     author.classList.add('author');
     number.classList.add('page-number');
-    readButton.classList.add('read');
+    readButton.classList.add('read-button');
+    deleteButton.classList.add('delete-button')
     buttonsContainer.classList.add('buttons-container');
-    title.textContent = this.title;
-    author.textContent = this.author;
-    number.textContent = this.number + ' pages';
-
-    if (this.read === 'true') {
-        readButton.textContent = 'read';
+    title.textContent = obj.title;
+    author.textContent = obj.author;
+    number.textContent = obj.number + ' pages';
+    if (obj.read === 'true') {
+        readButton.textContent = 'Read';
     } else {
-        readButton.textContent = 'not read';
+        readButton.textContent = 'Not read';
     }
-    
+    deleteButton.textContent = 'Delete';
+    deleteButton.onclick = function (e) {
+        e.target.parentElement.parentElement.remove();
+        let x = e.target.parentElement.parentElement.dataset.number;
+        myLibrary.splice(x,1);
+        let cards = document.querySelectorAll('.card');
+        let arr = Array.from(cards);
+        for (let card of cards) {
+            card.dataset.number = arr.indexOf(card)
+        }; 
+        console.log(myLibrary);
+    }
     buttonsContainer.appendChild(readButton);
+    buttonsContainer.appendChild(deleteButton);
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(number);
@@ -52,39 +67,45 @@ Book.prototype.displaybook = function () {
     cardsContainer.appendChild(card);
 }
 
-function displayBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        let card = document.createElement('div');
-        let title = document.createElement('div');
-        let author = document.createElement('div');
-        let number = document.createElement('div');
-        let buttonsContainer = document.createElement('div');
-        let read = document.createElement('button');
-        card.classList.add('card');
-        title.classList.add('title');
-        author.classList.add('author');
-        number.classList.add('page-number');
-        read.classList.add('read');
-        buttonsContainer.classList.add('buttons-container');
-        title.textContent = myLibrary[i].title;
-        author.textContent = myLibrary[i].author;
-        number.textContent = myLibrary[i].number + ' pages';
-        read.textContent = myLibrary[i].read;
-        buttonsContainer.appendChild(read);
-        card.appendChild(title);
-        card.appendChild(author);
-        card.appendChild(number);
-        card.appendChild(buttonsContainer);
-        cardsContainer.appendChild(card);
-    }
-}
+// function displayBooks() {
+//     for (let i = 0; i < myLibrary.length; i++) {
+//         let card = document.createElement('div');
+//         let title = document.createElement('div');
+//         let author = document.createElement('div');
+//         let number = document.createElement('div');
+//         let buttonsContainer = document.createElement('div');
+//         let readButton = document.createElement('button');
+//         let deleteButton = document.createElement('button');
+//         card.classList.add('card');
+//         title.classList.add('title');
+//         author.classList.add('author');
+//         number.classList.add('page-number');
+//         readButton.classList.add('read-button');
+//         buttonsContainer.classList.add('buttons-container');
+//         title.textContent = myLibrary[i].title;
+//         author.textContent = myLibrary[i].author;
+//         number.textContent = myLibrary[i].number + ' pages';
+//         if (myLibrary[i].read === 'true') {
+//             readButton.textContent = 'Read';
+//         } else {
+//             readButton.textContent = 'Not read';
+//         }
+//         deleteButton.textContent = 'Delete';
+//         buttonsContainer.appendChild(readButton);
+//         buttonsContainer.appendChild(deleteButton);
+//         card.appendChild(title);
+//         card.appendChild(author);
+//         card.appendChild(number);
+//         card.appendChild(buttonsContainer);
+//         cardsContainer.appendChild(card);
+//     }
+// }
 
-let bookOne = new Book('Apple', 'Hugo', '123', 'read');
-let bookTwo = new Book('Orange', 'Mac', '23', 'not read');
+let bookOne = new Book('Apple', 'Hugo', '123', 'true');
+let bookTwo = new Book('Orange', 'Mac', '23', 'false');
 
-bookOne.addBookToLibrary();
-bookTwo.addBookToLibrary();
-displayBooks();
+addBookToLibrary(bookOne);
+addBookToLibrary(bookTwo);
 
 addBookButton.addEventListener("click", () => {
     dialog.showModal();
@@ -105,7 +126,5 @@ function showResult(event) {
     dialog.close();
     readInput.value = readInput.checked ? !read : read;
     let book = new Book(titleInput.value, authorInput.value, pageNumberInput.value, readInput.value);
-    book.addBookToLibrary();
-    book.displaybook();
-    console.log(myLibrary);
+    addBookToLibrary(book);
 }
